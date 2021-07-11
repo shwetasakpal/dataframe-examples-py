e38578c68ed697d4c6aaa32c850477eb1381f71b
Shweta <shwetasakpal1993@gmail.com>
2021-07-11 21:18:36 +0530

    test

--- dataframe/ingestion/others/systems/mysql_df.py
+++ dataframe/ingestion/others/systems/mysql_df.py
@@ -29,7 +29,6 @@ if __name__ == '__main__':
     jdbc_params = {"url": ut.get_mysql_jdbc_url(app_secret),
                   "lowerBound": "1",
                   "upperBound": "100",
-                  "dbtable": app_conf["mysql_conf"]["query"],
                   "numPartitions": "2",
                   "partitionColumn": app_conf["mysql_conf"]["partition_column"],
                   "user": app_secret["mysql_conf"]["username"],
@@ -39,12 +38,15 @@ if __name__ == '__main__':
 
     # use the ** operator/un-packer to treat a python dictionary as **kwargs
     print("\nReading data from MySQL DB using SparkSession.read.format(),")
-    txnDF = spark\
-        .read.format("jdbc")\
-        .option("driver", "com.mysql.cj.jdbc.Driver")\
-        .options(**jdbc_params)\
-        .load()
+    for query in app_conf["mysql_conf"]["query_list"]:
+        jdbc_params['dbtable'] = query
+        print('Query:', query)
+        txnDF = spark\
+            .read.format("jdbc")\
+            .option("driver", "com.mysql.cj.jdbc.Driver")\
+            .options(**jdbc_params)\
+            .load()
 
-    txnDF.show()
+        txnDF.show()
 
 # spark-submit --packages "mysql:mysql-connector-java:8.0.15" dataframe/ingestion/others/systems/mysql_df.py
